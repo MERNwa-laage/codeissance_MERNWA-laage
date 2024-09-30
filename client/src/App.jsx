59a9e546fb4565facab0a0ae100db9f68446a83e
WannabeDeva <devanshukadam2003@gmail.com>
2024-09-30 13:21:47 +0530

    login

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,9 @@
-import { Button } from "@/components/ui/button"
-import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/clerk-react";
+
 
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Home from "./components/pages/Home"
 import Dashboard from "./components/pages/Dashboard"
+import Login from './components/pages/Login'
 
 
 const appRouter = createBrowserRouter([
@@ -14,6 +14,10 @@ const appRouter = createBrowserRouter([
   {
     path:'/dashboard',
     element:<Dashboard/>
+  },
+  {
+    path:'/login',
+    element:<Login/>
   },
 ])
 
