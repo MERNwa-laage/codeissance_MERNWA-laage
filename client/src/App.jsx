6026b64ea7b343aa193cd418701975915281bee2
yvrjgithub <127379082+yvrjgithub@users.noreply.github.com>
2024-10-01 01:05:52 +0530

    love sam

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,45 +1,27 @@
-
-
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Home from "./components/pages/Home"
-import Dashboard from "./components/pages/Dashboard"
-import Flights from './components/pages/Flights'
-import Login from './components/pages/Login'
-import Hotels from './components/pages/Hotels'
-import CreateTrip from './components/pages/CreateTrip'
-
+import React from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from './components/pages/Home';
+import Dashboard from './components/pages/Dashboard';
+import Flights from './components/pages/Flights';
+import Login from './components/pages/Login';
+import Hotels from './components/pages/Hotels';
+import CreateTrip from './components/pages/CreateTrip';
+import VoiceNavigation from './components/VoiceNavigation'; // Ensure correct path
 
 const appRouter = createBrowserRouter([
-  {
-    path:'/',
-    element:<Home/>
-  },
-  {
-    path:'/dashboard',
-    element:<Dashboard/>
-  },
-  {
-    path:'/flights',
-    element:<Flights/>
-  },
-  {
-    path:'/login',
-    element:<Login/>
-  },
-  {
-    path:'/hotels',
-    element:<Hotels/>
-  },
-  {
-    path:'/createtrip',
-    element:<CreateTrip/>
-  },
-])
+  { path: '/', element: <Home /> },
+  { path: '/dashboard', element: <Dashboard /> },
+  { path: '/flights', element: <Flights /> },
+  { path: '/login', element: <Login /> },
+  { path: '/hotels', element: <Hotels /> },
+  { path: '/createtrip', element: <CreateTrip /> },
+]);
 
 export default function App() {
   return (
     <div>
-      <RouterProvider router={appRouter}/>
+      <VoiceNavigation /> {/* This component listens for voice commands */}
+      <RouterProvider router={appRouter} />
     </div>
-  )
+  );
 }
